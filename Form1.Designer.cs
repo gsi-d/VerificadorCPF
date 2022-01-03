c16c66dd77a168d7a6fa6c682a08ce6d582b63e8
Guilherme <guimota_silver@hotmail.com>
2022-01-03 15:51:07 -0300

    Valida o CPF

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ValidaCPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ttbCPF = new System.Windows.Forms.MaskedTextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ttbCPF
+            // 
+            this.ttbCPF.Location = new System.Drawing.Point(108, 33);
+            this.ttbCPF.Mask = "000,000,000-00";
+            this.ttbCPF.Name = "ttbCPF";
+            this.ttbCPF.Size = new System.Drawing.Size(100, 23);
+            this.ttbCPF.TabIndex = 0;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(48, 73);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(220, 44);
+            this.btnValidar.TabIndex = 1;
+            this.btnValidar.Text = "Validar CPF";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 139);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.ttbCPF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox ttbCPF;
+        private System.Windows.Forms.Button btnValidar;
+    }
+}
+
